c6f9dd330f79968f1f747ea4708cbf16329c5a88
Gaurav <gauravkumaya996@gmail.com>
2024-07-18 10:18:57 +0530

    FitnessApp Initial Design
    FitnessApp Initial Design

---
+++ FitnessApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace FitnessApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int totalSteps = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            //count++;
+
+            //if (count == 1)
+            //    CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //    CounterBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnLogStepsClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(StepsEntry.Text, out int steps))
+            {
+                totalSteps += steps;
+                StepsLabel.Text = $"{totalSteps} steps";
+                StepsEntry.Text = string.Empty;
+            }
+            else
+            {
+                DisplayAlert("Invalid Input", "Please enter a valid number of steps.", "OK");
+            }
+        }
+
+    }
+
+}
---
+++ FitnessApp/UserFitData.xaml.cs
@@ -0,0 +1,15 @@
+namespace FitnessApp;
+
+public partial class UserFitData : ContentPage
+{
+	public UserFitData()
+	{
+		InitializeComponent();
+        LoadFitnessData();
+	}
+
+    private void LoadFitnessData()
+    {
+       
+    }
+}
